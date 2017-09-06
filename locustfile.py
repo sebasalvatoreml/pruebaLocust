438569237681a2fc2a15b70c12d06c3429f297b3
Sebastian Salvatore <sebastian.salvatore@mercadolibre.com>
2017-09-06 10:41:46 -0300

    config file added

--- locustfile.py
+++ locustfile.py
@@ -1,29 +1,35 @@
 from locust import HttpLocust, TaskSet, task
 from random import randint
+import json
+
+with open('config.json') as json_data:
+    config = json.load(json_data)
 
 def search(l):
-    response = l.client.get("site_id=MLA")
+    response = l.client.get("site_id=MCO", name="search")
     if response.status_code != 200: print "search: Response status code: " + str(response.status_code)
 
 def searchON(l):
-    response = l.client.get("site_id=MCO&marketplace=MELI")
+    response = l.client.get("site_id=MCO&marketplace=MELI", name="searchON")
     if response.status_code != 200: print "searchON: Response status code: " + str(response.status_code)
 
-def searchWithMarketplaceBins(l):
-    url = "site_id=MLA&marketplace=MELI&limit=100&bins="+str(randint(400000, 499999))
-    response = l.client.get(url, name="/site_id=MLA&marketplace=MELI&limit=100&bins=randomvisa")
-    if response.status_code != 200: print "searchWithMarketplaceBins: Response status code: " + str(response.status_code)
-    #print url
+def searchOFF(l):
+    url = "site_id=MCO&marketplace=MELI&limit=100&bins="+str(randint(400000, 499999))
+    response = l.client.get(url, name="searchOFF")
+    if response.status_code != 200: print "searchOFF: Response status code: " + str(response.status_code)
+
+def searchSandbox(l):
+    response = l.client.get("site_id=MLA&access_token="+config['testToken'], name="searchSandbox")
+    if response.status_code != 200: print "searchSandbox: Response status code: " + str(response.status_code)
 
 class searchBySiteId(TaskSet):
-    tasks = {search: 1, searchWithMarketplaceBins: 2, searchON: 3,}
+    tasks = {search: 1, searchON: 2, searchOFF: 3, searchSandbox: 4,}
 
     def on_start(self):
         search(self)
 
 class WebsiteSearch(HttpLocust):
     task_set = searchBySiteId
-    #host = "sandbox-sc.payment-methods.melifrontends.com/v1/payment_methods/search?"
-    host = "https://api.mercadopago.com/fury/payment_methods/search?"
+    host = config['host']
     min_wait = 1000
     max_wait = 2000
